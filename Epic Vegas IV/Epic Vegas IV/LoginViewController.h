474da3c2c4255c9f29649fc2c373e00f9036b603
zkohl <zkohl@Zachs-MacBook-Pro.local>
2014-08-04 16:50:15 -0700

    Added Title

---
+++ Epic Vegas IV/Epic Vegas IV/LoginViewController.h
@@ -0,0 +1,17 @@
+//
+//  LoginViewController.h
+//  Epic Vegas IV
+//
+//  Created by Zach on 8/4/14.
+//  Copyright (c) 2014 Zach Kohl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *epicLabel;
+@property (weak, nonatomic) IBOutlet UILabel *vegasLabel;
+@property (weak, nonatomic) IBOutlet UILabel *ivLabel;
+
+
+@end
---
+++ Epic Vegas IV/Epic Vegas IV/LoginViewController.m
@@ -0,0 +1,62 @@
+//
+//  LoginViewController.m
+//  Epic Vegas IV
+//
+//  Created by Zach on 8/4/14.
+//  Copyright (c) 2014 Zach Kohl. All rights reserved.
+//
+
+#import "LoginViewController.h"
+
+@interface LoginViewController ()
+
+@end
+
+@implementation LoginViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)setupTitleFont
+{
+    // set font for epic vegas labels
+    NSString* fontName = @"BoldDotDigital-7";
+    int fontSize = 74;
+    _epicLabel.font = [UIFont fontWithName:fontName
+                                      size:fontSize];
+    _vegasLabel.font = [UIFont fontWithName:fontName size:fontSize];
+    _ivLabel.font = [UIFont fontWithName:fontName size:fontSize];
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    // Do any additional setup after loading the view.    
+    [self setupTitleFont];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
