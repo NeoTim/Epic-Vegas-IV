49c60aa1c41a28c18defaa23750f61d0609bf94c
zkohl <zkohl@Zachs-MacBook-Pro.local>
2014-08-04 16:19:38 -0700

    Initial Checkin

---
+++ Epic Vegas IV/Epic Vegas IV/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Epic Vegas IV
+//
+//  Created by Zach on 8/4/14.
+//  Copyright (c) 2014 Zach Kohl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Epic Vegas IV/Epic Vegas IV/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Epic Vegas IV
+//
+//  Created by Zach on 8/4/14.
+//  Copyright (c) 2014 Zach Kohl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
