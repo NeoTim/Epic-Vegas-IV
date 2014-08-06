8a4f5d7916b7752293995e7c611c337774ee940f
zkohl <zkohl@zachs-mbp.wlan.qualcomm.com>
2014-08-05 20:22:17 -0700

    Setting up other view controllers and tab control

---
+++ Epic Vegas IV/Epic Vegas IV/TabBarViewController.h
@@ -0,0 +1,13 @@
+//
+//  TabBarViewController.h
+//  Epic Vegas IV
+//
+//  Created by Zach on 8/5/14.
+//  Copyright (c) 2014 Zach Kohl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TabBarViewController : UITabBarController
+
+@end
