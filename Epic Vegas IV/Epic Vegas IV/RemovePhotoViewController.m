555cb600f541288f6a712d020a7493b50fa2ceee
zkohl <zkohl@Zachs-MacBook-Pro.local>
2014-08-12 00:25:58 -0700

    aspect ratio is kept for the remove photo view

--- Epic Vegas IV/Epic Vegas IV/RemovePhotoViewController.m
+++ Epic Vegas IV/Epic Vegas IV/RemovePhotoViewController.m
@@ -28,6 +28,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
+    _attachedImageView.contentMode = UIViewContentModeScaleAspectFit;
     self.attachedImageView.image = _imageToShow;
 }
 
