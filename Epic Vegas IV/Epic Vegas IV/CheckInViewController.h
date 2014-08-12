26d4c22df635399125dd19b224f2e9db3191fd3f
zkohl <zkohl@zachs-mbp.wlan.qualcomm.com>
2014-08-12 15:55:00 -0700

    Check in view prototype complete

---
+++ Epic Vegas IV/Epic Vegas IV/CheckInViewController.h
@@ -0,0 +1,19 @@
+//
+//  CheckInViewController.h
+//  Epic Vegas IV
+//
+//  Created by Zach on 8/12/14.
+//  Copyright (c) 2014 Zach Kohl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
+
+@interface CheckInViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
+@property (weak, nonatomic) IBOutlet UITextField *locationNameTextField;
+
+- (IBAction)cancelButtonPressed:(id)sender;
+
+@end
---
+++ Epic Vegas IV/Epic Vegas IV/CheckInViewController.m
@@ -0,0 +1,57 @@
+//
+//  CheckInViewController.m
+//  Epic Vegas IV
+//
+//  Created by Zach on 8/12/14.
+//  Copyright (c) 2014 Zach Kohl. All rights reserved.
+//
+
+#import "CheckInViewController.h"
+
+@interface CheckInViewController ()
+
+@end
+
+@implementation CheckInViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+-(void)viewDidAppear:(BOOL)animated
+{
+    [_locationNameTextField becomeFirstResponder];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (IBAction)cancelButtonPressed:(id)sender {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+@end
